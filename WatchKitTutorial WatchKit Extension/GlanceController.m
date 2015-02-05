8bcfb1339729ea397bfbaf65a1bd6d8973baf845
danielle vass <danielle@de-velopment.com>
2015-02-05 17:39:17 +0000

    set up glances

--- WatchKitTutorial WatchKit Extension/GlanceController.m
+++ WatchKitTutorial WatchKit Extension/GlanceController.m
@@ -11,6 +11,9 @@
 
 @interface GlanceController()
 
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *lblTitle;
+@property (weak, nonatomic) IBOutlet WKInterfaceGroup *group;
+@property (nonatomic, strong) NSTimer *timer;
 @end
 
 
@@ -20,6 +23,55 @@
     [super awakeWithContext:context];
 
     // Configure interface objects here.
+    
+   
+    
+    [self getUserDefaults];
+    
+    //schedule a time to check every 5 seconds
+    _timer = [NSTimer scheduledTimerWithTimeInterval:5 target:self selector:@selector(getUserDefaults) userInfo:nil repeats:YES];
+    
+    
+}
+
+-(void) setColour:(NSString *) textColour{
+
+    UIColor *color;
+    
+    if ([textColour isEqualToString:@"Red"]) {
+        color = [UIColor redColor];
+    } else if ([textColour isEqualToString:@"Orange"]) {
+        color = [UIColor orangeColor];
+    } else if ([textColour isEqualToString:@"Yellow"]) {
+        color = [UIColor yellowColor];
+    } else if ([textColour isEqualToString:@"Green"]) {
+        color = [UIColor greenColor];
+    } else if ([textColour isEqualToString:@"Blue"]) {
+        color = [UIColor blueColor];
+    } else if ([textColour isEqualToString:@"Purple"]) {
+        color = [UIColor purpleColor];
+    } else {
+        color = [UIColor darkGrayColor];
+    }
+    
+    [_group setBackgroundColor:color];
+}
+
+-(void) getUserDefaults
+{
+    //use our group user defaults
+    NSUserDefaults *defaults = [[NSUserDefaults alloc] initWithSuiteName:@"group.com.calvium.watch.dev.defaults"];
+    
+    //get the colour
+    NSString *textColour = [defaults objectForKey:@"colour"];
+    
+    if (textColour) {
+        [_lblTitle setText:textColour];
+        [self setColour:textColour];
+    } else {
+        [_lblTitle setText:@"not set yet"];
+        [_group setBackgroundColor:[UIColor darkGrayColor]];
+    }
 }
 
 - (void)willActivate {
@@ -30,6 +82,9 @@
 - (void)didDeactivate {
     // This method is called when watch view controller is no longer visible
     [super didDeactivate];
+    
+    //kill our timer!
+    [_timer invalidate];
 }
 
 @end
